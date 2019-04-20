c1a82544406dd89f97136608b6c14064349464f3
Paul Steele <paul-steele@live.com>
2019-04-20 17:23:11 -0400

    add more polybar + i3 gaps

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -8,10 +8,23 @@ while pgrep -x polybar >/dev/null; do sleep 1; done
 
 # Launch polybar
 DISPLAY1="$(xrandr -q | grep 'eDP1\|VGA-1\|rdp2\|HDMI2' | cut -d ' ' -f1)"
-[[ ! -z "$DISPLAY1" ]] && MONITOR="$DISPLAY1" polybar bar &
-
 DISPLAY2="$(xrandr -q | grep 'HDMI1\|VGA-2\|rdp1' | cut -d ' ' -f1)"
-[[ ! -z $DISPLAY2 ]] && MONITOR=$DISPLAY2 polybar bar &
-
 DISPLAY3="$(xrandr -q | grep 'rdp0' | cut -d ' ' -f1)"
-[[ ! -z $DISPLAY3 ]] && MONITOR=$DISPLAY3 polybar bar &
+
+if [[ ! -z "$DISPLAY1" ]]; then     
+  export MONITOR="$DISPLAY1" 
+  polybar top & 
+  polybar bottom &
+fi
+
+if [[ ! -z $DISPLAY2 ]]; then
+  export MONITOR=$DISPLAY2 
+  polybar top & 
+  polybar bottom &
+fi
+
+if [[ ! -z $DISPLAY3 ]]; then 
+  export MONITOR=$DISPLAY3 
+  polybar top & 
+  polybar bottom &
+fi
