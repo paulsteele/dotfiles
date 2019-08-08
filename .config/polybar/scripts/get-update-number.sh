e9714caa2b8254e51c2c0a7bbb5f99ae9fe55227
Paul Steele <paul_steele@live.com>
2019-08-08 17:38:16 -0400

    update network / updates for arch

--- .config/polybar/scripts/get-update-number.sh
+++ .config/polybar/scripts/get-update-number.sh
@@ -1,12 +1,16 @@
 #!/bin/sh
-if [ -x "$(command -v yay)" ]; then
-  PACKAGES="Undefined"
+if [ -x "$(command -v checkupdates)" ] && [ -x "$(command -v yay)" ]; then
+  PACKAGES="$(checkupdates 2> /dev/null | wc -l)"
+  PACKAGES=$((PACKAGES + $(yay -Qum 2> /dev/null | wc -l)))
 elif [ -x "$(command -v apt)" ]; then
   # Suppress error from apt about cli being unstable
   # This method is much simpler / faster than parsing apt-get
   # and fairly easy to tell if there is an issue
   PACKAGES=$(apt list -u 2>/dev/null | wc -l)
+  # Remove header line
   PACKAGES=$((PACKAGES - 1))
+else
+  PACKAGES="Can't be determined"
 fi
 
 echo "$PACKAGES"
