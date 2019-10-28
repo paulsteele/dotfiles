56c579c648c862cd8f1a7ac781fa7777b5543f64
Paul Steele <paul_steele@live.com>
2019-10-28 15:43:00 -0400

    add 3rd monitor + spellings

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -9,7 +9,7 @@ while pgrep -x polybar >/dev/null; do sleep 1; done
 # Launch polybar
 DISPLAY1="$(xrandr -q | grep 'eDP1\|VGA-1\|rdp2\|HDMI2\|Virtual1' | cut -d ' ' -f1)"
 DISPLAY2="$(xrandr -q | grep 'HDMI1\|VGA-2\|rdp1\|Virtual2' | cut -d ' ' -f1)"
-DISPLAY3="$(xrandr -q | grep 'rdp0' | cut -d ' ' -f1)"
+DISPLAY3="$(xrandr -q | grep 'rdp0\|VGA-3' | cut -d ' ' -f1)"
 
 if [[ ! -z "$DISPLAY1" ]]; then     
   export MONITOR="$DISPLAY1" 
