d5ebdc15a927d00b03eabdcbff2c4facfcb4505f
Paul Steele <paul@paul-steele.com>
2026-05-09 19:57:43 -0400

    more hyper

---
+++ .config/hypr/screen-on.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+HEADLESS=$(hyprctl -j monitors | jq -r '.[] | select(.name | test("HEADLESS-"; "i")).name')
+
+hyprctl keyword monitor DP-1,enable
+
+hyprctl keyword monitor DP-1,3840x2160@119.88Hz,auto,1,vrr,3
+
+hyprctl output remove "$HEADLESS"
