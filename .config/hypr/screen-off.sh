710f23c91712bc6bbd0edc95d58783ebe9e67a6d
Paul Steele <paul@paul-steele.com>
2026-06-16 18:20:40 -0400

    update screen-on & screen-off

--- .config/hypr/screen-off.sh
+++ .config/hypr/screen-off.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 hyprctl output create headless
+sleep 0.2
 HEADLESS=$(hyprctl -j monitors | jq -r '.[] | select(.name | test("HEADLESS-"; "i")).name')
-hyprctl keyword monitor $HEADLESS,3840x2160@119.88Hz,auto,1,vrr,3
+[ -z "$HEADLESS" ] && exit 1
+
+hyprctl keyword monitor "$HEADLESS",3840x2160@119.88Hz,auto,1,vrr,3
 
 hyprctl keyword monitor DP-1,disable
--- .config/hypr/screen-on.sh
+++ .config/hypr/screen-on.sh
@@ -2,8 +2,6 @@
 
 HEADLESS=$(hyprctl -j monitors | jq -r '.[] | select(.name | test("HEADLESS-"; "i")).name')
 
-hyprctl keyword monitor DP-1,enable
-
 hyprctl keyword monitor DP-1,3840x2160@119.88Hz,auto,1,vrr,3
 
 hyprctl output remove "$HEADLESS"
