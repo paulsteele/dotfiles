ceabdfbd1bbf8d826eea249280f42820c511f4ca
Paul Steele <paul@paul-steele.com>
2026-06-18 16:46:03 -0400

    update screen off

--- .config/hypr/screen-off.sh
+++ .config/hypr/screen-off.sh
@@ -1,4 +1,8 @@
 #!/usr/bin/env bash
+
+# Remember the current workspace so we can return to it
+ACTIVE_WORKSPACE=$(hyprctl -j activeworkspace | jq -r '.name')
+
 hyprctl output create headless
 sleep 0.2
 HEADLESS=$(hyprctl -j monitors | jq -r '.[] | select(.name | test("HEADLESS-"; "i")).name')
@@ -7,3 +11,6 @@ HEADLESS=$(hyprctl -j monitors | jq -r '.[] | select(.name | test("HEADLESS-"; "
 hyprctl keyword monitor "$HEADLESS",3840x2160@119.88Hz,auto,1,vrr,3
 
 hyprctl keyword monitor DP-1,disable
+
+# Restore focus to the workspace we were on
+hyprctl dispatch workspace "$ACTIVE_WORKSPACE"
